b0961019b632285a30f2840dde0a64447879311e
Jane Kim <Janeekim12@gmail.com>
2018-01-31 22:49:13 -0700

    iteration3

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,42 +6,62 @@ class LinkedList
   def initialize
    @head = nil
    @node_count = 0
-   @name_in_list = []
+
   end
 
   def append(name)
-    @node_count +=1
-    @name_in_list << name
-    @head = Node.new(name)
-  end
+      current = @head
+    if @head == nil
+       @head = Node.new(name)
 
-  def tail(current_node = @head)
-    if current_node.next_node != nil
-       current_node = current_node.next_node
-       tail(current_node)
     else
-      tail
+      until current.next_node == nil
+            current = current.next_node
+      end
+         current.next_node = Node.new(name)
     end
   end
+
+    # @node_count +=1
+    # # @name_in_list << name
+    # @head = Node.new(name)
+
+
+  # def tail(current_node = @head)
+  #   if current_node.next_node != nil
+  #      current_node = current_node.next_node
+  #      tail(current_node)
+  #   else
+  #     current_node
+  #   end
+  # end
 
   def count
-    my_counter = 0
-    if @head.next_node.nil?
-      my_counter = 1
+    if @head == nil
+      my_counter = 0
+
     else
       current_node = @head
-      until current_node.next_node = nil
-        my_counter +=1
+      my_counter = 1
+
+      until current_node == nil
         current_node = current_node.next_node
 
+        my_counter +=1
       end
+      my_counter
     end
- my_counter
+
   end
 
   def to_string
-    first_name = @name_in_list[0]
-    "The #{first_name} family"
+    current_node = @head.next_node
+    family_name = "The #{@head.surname} family"
+    until current_node.next_node == nil do
+    family_name << ", followed by the #{current_node.surname} family"
+      current_node = current_node.next_node
+    end
+    family_name
   end
 
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -17,6 +17,14 @@ class LinkedListTest <Minitest::Test
     assert_instance_of Node, list.append("West")
   end
 
+  def test_append_works_more_than_once
+    list = LinkedList.new
+    list.append("West")
+    list.append("East")
+    assert_instance_of Node, list.append("West")
+
+end
+
      def test_next_node_is_nil
        list = LinkedList.new
        list.append("West")
@@ -26,17 +34,18 @@ class LinkedListTest <Minitest::Test
      def test_count_elements_in_list
        list = LinkedList.new
        list.append("West")
-       assert_equal list.count,1
+       assert_equal 1, list.count
        list.append("South")
        list.append("North")
-       binding.pry
-       assert_equal list.count,3
+       assert_equal 3, list.count
      end
 
      def test_list_into_string
        list = LinkedList.new
-       list.append("West")
+       list.append("Rhodes")
        list.append("Hardy")
-       assert_equal list.to_string,"The West family"
+       list.append("Mckinney")
+       binding.pry
+       assert_equal "The Rhodes family, followed by the Hardy family, followed by the Mckinney family", list.to_string
    end
  end
